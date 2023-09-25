4a343a11a4970c93800f04536114026f073a5eb9
zwiersm <143877404+zwiersm@users.noreply.github.com>
2023-09-25 11:03:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,22 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector('.menu');
+var kruisje = document.querySelector ('.closemenu');
+var heleNav = document.querySelector('nav');
+
+heleNav.classList.add('hidden');
+
+function openMenu() {
+    heleNav.classList.remove('hidden');
+    menuButton.classList.add('hidden')
+}
+
+function closeMenu() {
+    heleNav.classList.add('hidden');
+    menuButton.classList.remove('hidden')
+}
+
+menuButton.addEventListener("click", openMenu);
+
+kruisje.addEventListener("click", closeMenu);
